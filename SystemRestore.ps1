a172b7da2940385525def370aaafa5d8fcf37d9e
Travis Pratt <tpratt1994@gmail.com>
2022-07-28 14:23:55 -0500

    Turns on system Restore based on computer list through invoke-command

---
+++ SystemRestore.ps1
@@ -0,0 +1,16 @@
+﻿#This list will turn on system restore protection on the machine. This does require Administrator priveledges and a txt file with a list of computers separated by new lines.
+#You can modify the drives by adding the drive letter separated by a ,.
+function enableRestore{
+    [CmdletBinding()]
+    param ()
+    
+    Enable-ComputerRestore -drive "C:"
+    vssadmin resize shadowstorage /on=c: /for=c: /maxsize=10%
+}
+$List = Get-Content -Path C:\temp\computers.txt
+$cred = Get-Credential
+
+foreach($item in $list){
+    Invoke-Command -ComputerName $item -Credential $cred -ScriptBlock ${function:enableRestore}
+}
+
